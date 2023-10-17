85c7a4ff680cb216a87a6105d1a862b848a4120d
Oleksii <aleksejlitovcenko69@gmail.com>
2023-10-17 18:06:58 +0300

    model for portfolio

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PortfolioItem(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    position = models.PositiveSmallIntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+    slug = models.CharField(max_length=50, unique=True, db_index=True)
+
