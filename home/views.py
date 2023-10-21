53e600d1343ffbdfeba4cdb1f7fc7b1158d7a607
Oleksii <aleksejlitovcenko69@gmail.com>
2023-10-21 20:19:00 +0300

    .

--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from .models import PortfolioItem
 # Create your views here.
 def view_homepage(request):
     portfolioitem = PortfolioItem.objects.filter(is_visible=True)
-    return render(request, 'index.html', context={'portfolioitem': portfolioitem})
+    return render(request, 'main.html', context={'portfolioitem': portfolioitem})
