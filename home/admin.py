c258db27e9dff0e9d9846d6d08c592011f4e387c
Oleksii <aleksejlitovcenko69@gmail.com>
2023-10-24 17:50:32 +0300

    new models

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import PortfolioItem
+from .models import PortfolioItem, About, Services, Team
 # Register your models here.
 admin.site.register(PortfolioItem)
+admin.site.register(About)
+admin.site.register(Services)
+admin.site.register(Team)
+
+
--- home/views.py
+++ home/views.py
@@ -5,4 +5,5 @@ from .models import PortfolioItem
 # Create your views here.
 def view_homepage(request):
     portfolioitem = PortfolioItem.objects.filter(is_visible=True)
-    return render(request, 'index.html', context={'portfolioitem': portfolioitem})
+
+    return render(request, 'index.html', context={'portfolioitem': portfolioitem, })
